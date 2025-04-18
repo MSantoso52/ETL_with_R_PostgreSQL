c3dc06124be8d3230641b2c1952aefa46c8dd3e7
mulyo <mulyo@rockylinux.lan>
2025-04-18 14:42:23 +0700

    Add R for ETL procress

---
+++ postgres_etl_1.R
@@ -0,0 +1,53 @@
+# Load necessary library
+library(RPostgres)
+library(dplyr)
+
+# Extract
+data(mtcars)
+cars_df <- as.data.frame(mtcars)
+
+# Transform
+cars_transformed <- cars_df %>%
+  # Remove duplicate
+  distinct() %>%
+  #  Remove any NA values
+  na.omit() %>%
+  # Ensure the format are correct:
+  mutate(
+    mpg = as.numeric(mpg),
+    cyl = as.integer(cyl),
+    disp = as.numeric(disp),
+    hp = as.numeric(hp),
+    drat = as.numeric(drat),
+    wt = as.numeric(wt),
+    qsec = as.numeric(qsec),
+    vs = as.integer(vs),
+    am = as.integer(am),
+    gear = as.integer(gear),
+    carb = as.integer(carb)
+  )
+
+# View the transformes data
+print(cars_transformed)
+
+# Load 
+# Database connection
+con <-dbConnect(
+  RPostgres::Postgres(),
+  host = "localhost",
+  dbname = "carsdata",
+  user = "mulyo",
+  password = "Hasegawa@053",
+  port = 5432
+)
+
+dbWriteTable(
+  con,
+  "mtcars",
+  cars_transformed, 
+  overwrite = TRUE,
+  row.names = TRUE
+)
+
+# Close the database connection
+dbDisconnect(con)
